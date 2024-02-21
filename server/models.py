6d303c92d49d4d5662c0266e071202c10894e25f
Alan Silva <silzen.ent@gmail.com>
2024-02-20 23:39:28 -0700

    Completed Codealong: Flask-SQLAlc 1 Intro

--- server/models.py
+++ server/models.py
@@ -2,17 +2,26 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
 # contains definitions of tables and associated schema constructs
+# read more about Metadata using the link at the bottom of the page
 metadata = MetaData()
 
 # create the Flask SQLAlchemy extension
 db = SQLAlchemy(metadata=metadata)
 
 # define a model class by inheriting from db.Model.
-
-
 class Pet(db.Model):
     __tablename__ = 'pets'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True,
+                         nullable=False, index=True)
+    email = db.Column(db.String(120), unique=True)
+    verified = db.Column(db.Boolean, default=False)
